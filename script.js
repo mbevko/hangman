90ebc78df97a896e662873465dc93faa15d29b35
Michael <49693341+mbevko@users.noreply.github.com>
2020-12-04 18:58:22 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -11,21 +11,21 @@ let gallow = document.querySelectorAll("g .gallow")
 let win = document.querySelector(".win");
 let lose = document.querySelector(".lose");
 let hangingGuy = document.querySelectorAll("g rect");
+let categories = document.querySelectorAll(".categories button")
+let allKeys = document.querySelector(".all_keys");
 
-
-const wordList = ["empire", "lightsaber", "wookiee", "mandalorian", "jedi", "stormtrooper", "force", "droid", "ewoks", "sith", "padawan", "yoda", "tatooine", "kashyyyk", "blaster", "clone"];
-
-guessBtn.forEach(letter => {
+const starWars = ["empire", "lightsaber", "wookiee", "mandalorian", "jedi", "stormtrooper", "force", "droid", "ewoks", "sith", "padawan", "yoda", "tatooine", "kashyyyk", "blaster", "clone", "kenobi", "windu", "skywalker", "grogu", "solo", "bantha", "grievous", "bespin", "hoth", "dagobah", "endor", "leia", "tarkin", "anakin", "tauntaun", "rebellion", "alderaan", "naboo", "jabba", "corellia", "apprentice", "darth", "vader", "sidious", "emperor", "master", "binks", "senator", "deathstar", "greedo", "bounty", "yavin"];
+const cities = ["madrid", "athens", "paris", "austin", "dallas", "philadelphia", "amsterdam", "thessaloniki", "marseille", "tokyo", "taipei", "bangkok", "mumbai", "london", "chicago", "berlin", "dubai", "moscow", "istanbul"]
+const animals = ["whale", "zebra", "jellyfish", "elephant", "cow", "lemur", "caterpillar", "toucan", "bison", "mustang", "hamster", "mole", "gecco", "wombat", "aardvaark", "anteater", "koala", "orangutan", "sloth", "cheetah"]
+guessBtn.forEach(letter => { 
     letter.setAttribute("disabled", "")
-})
+});
 
-
-
+let subject = ""
 let newWord = "";
-const wordSelector = () => {
-    let random = Math.floor(Math.random() * wordList.length);
-
-    newWord = wordList[random]
+const wordSelector = (category) => {
+    let random = Math.floor(Math.random() * category.length);
+    newWord = category[random]
     console.log(newWord)
 };
 
@@ -42,7 +42,6 @@ const blankSpaces = () => {
 //Deleting letter spaces
 const removeSpaces = () => {
     let currentWordArr = wordContainer.querySelectorAll(".test");
-
     currentWordArr.forEach(spaces => {
         spaces.remove()
     });
@@ -53,6 +52,7 @@ let wrongLetters = []
 let rightLetters = [];
 let finalWord = []
 
+//player guess functions
 guessBtn.forEach(letter => {
     letter.addEventListener('click', () => {
         let currentWord = newWord.split("")
@@ -71,6 +71,8 @@ guessBtn.forEach(letter => {
                     finalWord.push(letter.innerText)
                 }
             };
+
+
             if (finalWord.length === currentWord.length) {
                 win.style.display = "block"
                 guessBtn.forEach(button => {
@@ -98,6 +100,10 @@ guessBtn.forEach(letter => {
         } else if (wrongLetters.length === 8) {
             document.querySelector(".left_leg").style.display = "block";
             lose.style.display = "block";
+            for(let i = 0; i < currentWord.length; i++){
+                currentWordArr[i].innerHTML = currentWord[i].toUpperCase();
+                currentWordArr[i].style.color = "grey";
+            };
             guessBtn.forEach(button => {
                 button.setAttribute("disabled", "")
             })
@@ -105,11 +111,10 @@ guessBtn.forEach(letter => {
     })
 });
 
-//clear the board to start a new game
-newGame.addEventListener('click', () => {
+//choose a category and start a new game
+const categoryAll = () => {
     removeSpaces();
-    wordSelector();
-    let currentWordSplit = newWord.split("");
+    currentWordSplit = newWord.split("");
     blankSpaces()
     wrongLetters = [];
     rightLetters = [];
@@ -121,10 +126,38 @@ newGame.addEventListener('click', () => {
         piece.style.display = "none";
     });
     document.querySelector(".head").style.display = "none";
-
     guessBtn.forEach(letter => {
-        if (letter.hasAttribute("disabled", "")) {
-            letter.removeAttribute("disabled", "")
-        }
-    })
+      if (letter.hasAttribute("disabled", "")) {
+          letter.removeAttribute("disabled", "")
+      }
+  })
+};
+
+
+//category selections
+categories[0].addEventListener('click', () => {
+    wordSelector(animals);
+    categoryAll()
+    categories[0].style.cssText = "opacity: 1; border: solid #B9FFCE 2px";
+    categories[1].style.cssText = "opacity: .5; border: solid #B9FFCE 1px";
+    categories[2].style.cssText = "opacity: .5; border: solid #B9FFCE 1px";
 });
+
+categories[1].addEventListener('click', () => {
+  wordSelector(cities);
+  categoryAll();
+  categories[1].style.cssText = "opacity: 1; border: solid #B9FFCE 2px";
+  categories[0].style.cssText = "opacity: .5; border: solid #B9FFCE 1px";
+  categories[2].style.cssText = "opacity: .5; border: solid #B9FFCE 1px";
+});
+
+categories[2].addEventListener('click', () => {
+  wordSelector(starWars);
+  categoryAll();
+  categories[2].style.cssText = "opacity: 1; border: solid #B9FFCE 2px";
+  categories[0].style.cssText = "opacity: .5; border: solid #B9FFCE 1px";
+  categories[1].style.cssText = "opacity: .5; border: solid #B9FFCE 1px";
+});
+
+//have keyboard select keys
+//put category buttons in column and arrange keyboard under the hanging man
